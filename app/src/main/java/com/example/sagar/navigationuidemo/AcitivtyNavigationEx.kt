195f5c410d066f64372cb4ec2dfefbf3cfbe2b8c
lymons <liutao@fenrir-tec.com>
2019-01-15 11:17:08 +0800

    fixed the problem that the icon of top-left change to back/up when migrated to fragment of  drawerlayout

--- app/src/main/java/com/example/sagar/navigationuidemo/AcitivtyNavigationEx.kt
+++ app/src/main/java/com/example/sagar/navigationuidemo/AcitivtyNavigationEx.kt
@@ -6,10 +6,22 @@ import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.navigateUp
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
+@Suppress("UNUSED")
+fun Activity.navigateUp(@IdRes viewId: Int, @Nullable drawerLayout: DrawerLayout): Boolean
+        = navigateUp(Navigation.findNavController(this, viewId), drawerLayout)
 
-fun Activity.navigateUp(@IdRes viewId: Int, @Nullable drawerLayout: DrawerLayout): Boolean = navigateUp(Navigation.findNavController(this, viewId), drawerLayout)
+@Suppress("UNUSED")
+fun Activity.navigateUp(@IdRes viewId: Int, @Nullable configuration: AppBarConfiguration): Boolean
+        = navigateUp(Navigation.findNavController(this, viewId), configuration)
 
-fun AppCompatActivity.setupNavController(@IdRes viewId: Int, @Nullable drawerLayout: DrawerLayout) = setupActionBarWithNavController(this, Navigation.findNavController(this, viewId), drawerLayout)
+@Suppress("UNUSED")
+fun AppCompatActivity.setupNavController(@IdRes viewId: Int, @Nullable drawerLayout: DrawerLayout)
+        = setupActionBarWithNavController(this, Navigation.findNavController(this, viewId), drawerLayout)
+
+@Suppress("UNUSED")
+fun AppCompatActivity.setupNavController(@IdRes viewId: Int, @Nullable configuration: AppBarConfiguration)
+        = setupActionBarWithNavController(this, Navigation.findNavController(this, viewId), configuration)
--- app/src/main/java/com/example/sagar/navigationuidemo/MainActivity.kt
+++ app/src/main/java/com/example/sagar/navigationuidemo/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.sagar.navigationuidemo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +21,16 @@ class MainActivity : AppCompatActivity() {
         // Set up navigation menu
         navigationView.setupWithNavController(navController)
 
+        appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.bottomNavFragment, R.id.infoFragment),
+                drawerLayout)
+
         // Set up ActionBar
         setSupportActionBar(toolbar)
-        setupNavController(R.id.mainNavFragment, drawerLayout)
+        setupNavController(R.id.mainNavFragment, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navigateUp(R.id.mainNavFragment, drawerLayout)
+        return navigateUp(R.id.mainNavFragment, appBarConfiguration)
     }
 }
